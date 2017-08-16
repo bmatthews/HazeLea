1a77810cbd65e379451008c1edc9294734efaf2c
Ben.Matthews <Ben.Matthews@PARM2305>
2017-08-16 21:10:33 +0100

    added static file handling fro[B

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function(req, res) {
-  res.send('Hello World!')
-})
+app.set('port', process.env.PORT || 5000)
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!')
+app.use(express.static(__dirname + '/www'))
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'))
 })
